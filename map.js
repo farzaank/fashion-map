56ca30b212d985a3517a5bec4fecd38f8a1661f9
farzaank <fkaiyom@gmail.com>
2023-12-10 18:56:47 -0800

    updated map

--- map.js
+++ map.js
@@ -1,94 +1,531 @@
-// Load and process the data
-d3.csv("dataviza2.csv").then(function (data) {
-	// Dimensions and margins of the graph
-	const margin = { top: 10, right: 30, bottom: 30, left: 60 },
-		width = 800 - margin.left - margin.right,
-		height = 500 - margin.top - margin.bottom;
+const mapData = [
+	{
+		England: "60-69.9%",
+		Australia: "60-69.9%",
+		Netherlands: "60-69.9%",
+		Portugal: "60-69.9%",
+		USA: "60-69.9%",
+		Ireland: "60-69.9%",
+		Norway: "60-69.9%",
+		Spain: "60-69.9%",
+		Sweden: "60-69.9%",
+		Albania: "60-69.9%",
+		Austria: "60-69.9%",
+		Denmark: "60-69.9%",
+		France: "60-69.9%",
+		Georgia: "60-69.9%",
+		Germany: "60-69.9%",
+		Greece: "60-69.9%",
+		Canada: "60-69.9%",
+		Finland: "60-69.9%",
+		Montenegro: "60-69.9%",
+		Azerbaijan: "50-59.9%",
+		Belgium: "50-59.9%",
+		Croatia: "50-59.9%",
+		Czechia: "50-59.9%",
+		Italy: "50-59.9%",
+		Latvia: "50-59.9%",
+		Philippines: "50-59.9%",
+		Argentina: "50-59.9%",
+		"Bosnia and Herzegovina": "50-59.9%",
+		Cyprus: "50-59.9%",
+		Lithuania: "50-59.9%",
+		"North Macedonia": "50-59.9%",
+		Romania: "50-59.9%",
+		Slovakia: "50-59.9%",
+		Estonia: "50-59.9%",
+		Serbia: "50-59.9%",
+		Slovenia: "50-59.9%",
+		Uruguay: "50-59.9%",
+		Bahrain: "50-59.9%",
+		Bulgaria: "50-59.9%",
+		Chile: "50-59.9%",
+		Hungary: "50-59.9%",
+		Mexico: "50-59.9%",
+		Peru: "50-59.9%",
+		Poland: "50-59.9%",
+		Thailand: "50-59.9%",
+		Armenia: "50-59.9%",
+		"New Zealand": "50-59.9%",
+		Nigeria: "50-59.9%",
+		Bahamas: "50-59.9%",
+		"South Africa": "50-59.9%",
+		Brazil: "50-59.9%",
+		"Costa Rica": "50-59.9%",
+		Ecuador: "50-59.9%",
+		Guyana: "50-59.9%",
+		Jamaica: "50-59.9%",
+		Luxembourg: "50-59.9%",
+		Panama: "50-59.9%",
+		"Saint Lucia": "50-59.9%",
+		Ukraine: "50-59.9%",
+		Indonesia: "50-59.9%",
+		Rwanda: "50-59.9%",
+		Switzerland: "50-59.9%",
+		"United Arab Emirates": "50-59.9%",
+		Bangladesh: "40-49.9%",
+		Malta: "40-49.9%",
+		Paraguay: "40-49.9%",
+		Qatar: "40-49.9%",
+		"Saudi Arabia": "40-49.9%",
+		"Sri Lanka": "40-49.9%",
+		"Trinidad and Tobago": "40-49.9%",
+		Turkey: "40-49.9%",
+		Belarus: "40-49.9%",
+		Bolivia: "40-49.9%",
+		Guatemala: "40-49.9%",
+		Honduras: "40-49.9%",
+		Iceland: "40-49.9%",
+		Kyrgyzstan: "40-49.9%",
+		Moldova: "40-49.9%",
+		Singapore: "40-49.9%",
+		Tunisia: "40-49.9%",
+		Vietnam: "40-49.9%",
+		Taiwan: "40-49.9%",
+		Colombia: "40-49.9%",
+		"Dominican Republic": "40-49.9%",
+		Fiji: "40-49.9%",
+		India: "40-49.9%",
+		Jordan: "40-49.9%",
+		Kazakhstan: "40-49.9%",
+		Kenya: "40-49.9%",
+		Laos: "40-49.9%",
+		Uganda: "40-49.9%",
+		Uzbekistan: "40-49.9%",
+		Belize: "40-49.9%",
+		"El Salvador": "40-49.9%",
+		Ethiopia: "40-49.9%",
+		Ghana: "40-49.9%",
+		Israel: "40-49.9%",
+		Malaysia: "40-49.9%",
+		Mozambique: "40-49.9%",
+		Nepal: "40-49.9%",
+		"Saint Vincent and the Grenadines": "40-49.9%",
+		Zambia: "40-49.9%",
+		Kosovo: "40-49.9%",
+		Angola: "40-49.9%",
+		"Antigua and Barbuda": "40-49.9%",
+		Egypt: "40-49.9%",
+		Japan: "40-49.9%",
+		Namibia: "40-49.9%",
+		Botswana: "40-49.9%",
+		Cambodia: "40-49.9%",
+		Madagascar: "40-49.9%",
+		Myanmar: "40-49.9%",
+		Tanzania: "40-49.9%",
+		"United Republic of Tanzania": "40-49.9%",
+		Barbados: "40-49.9%",
+		Cameroon: "40-49.9%",
+		Liberia: "40-49.9%",
+		Benin: "30-39.9%",
+		China: "30-39.9%",
+		Lesotho: "30-39.9%",
+		Mongolia: "30-39.9%",
+		Morocco: "30-39.9%",
+		Senegal: "30-39.9%",
+		Malawi: "30-39.9%",
+		Oman: "30-39.9%",
+		"South Korea": "30-39.9%",
+		"Ivory Coast": "30-39.9%",
+		Haiti: "30-39.9%",
+		Kuwait: "30-39.9%",
+		Maldives: "30-39.9%",
+		Niger: "30-39.9%",
+		Pakistan: "30-39.9%",
+		Algeria: "30-39.9%",
+		"Burkina Faso": "30-39.9%",
+		"Democratic Republic of the Congo": "30-39.9%",
+		Djibouti: "30-39.9%",
+		Eswatini: "30-39.9%",
+		Gambia: "30-39.9%",
+		Guinea: "30-39.9%",
+		Mauritius: "30-39.9%",
+		Nicaragua: "30-39.9%",
+		Tajikistan: "30-39.9%",
+		"Brunei Darussalam": "30-39.9%",
+		Mauritania: "30-39.9%",
+		"Timor-Leste": "30-39.9%",
+		"Guinea-Bissau": "30-39.9%",
+		Iraq: "30-39.9%",
+		Lebanon: "30-39.9%",
+		Seychelles: "30-39.9%",
+		"Sierra Leone": "30-39.9%",
+		Togo: "30-39.9%",
+		"Hong Kong": "30-39.9%",
+		"Cabo Verde": "30-39.9%",
+		Cuba: "30-39.9%",
+		Liechtenstein: "30-39.9%",
+		Mali: "30-39.9%",
+		"Papua New Guinea": "30-39.9%",
+		"Solomon Islands": "30-39.9%",
+		Suriname: "30-39.9%",
+		Burundi: "20-29.9%",
+		"Republic of the Congo": "20-29.9%",
+		"Central African Republic": "20-29.9%",
+		Palau: "20-29.9%",
+		Venezuela: "20-29.9%",
+		Zimbabwe: "20-29.9%",
+		Sudan: "20-29.9%",
+		"South Sudan": "20-29.9%",
+		Turkmenistan: "20-29.9%",
+		Vanuatu: "20-29.9%",
+		Chad: "20-29.9%",
+		Gabon: "20-29.9%",
+		Russia: "20-29.9%",
+		"Equatorial Guinea": "20-29.9%",
+		Somalia: "10-19.9%",
+		Libya: "10-19.9%",
+		Iran: "<0-9.9%",
+		Eritrea: "<0-9.9%",
+		"North Korea": "<0-9.9%",
+	},
+	{
+		Netherlands: "60-69.9%",
+		USA: "60-69.9%",
+		England: "60-69.9%",
+		Sweden: "60-69.9%",
+		Australia: "60-69.9%",
+		Portugal: "60-69.9%",
+		Croatia: "60-69.9%",
+		Spain: "60-69.9%",
+		Belgium: "60-69.9%",
+		Norway: "60-69.9%",
+		Austria: "60-69.9%",
+		Argentina: "40-49.9%",
+		Germany: "50-59.9%",
+		Denmark: "50-59.9%",
+		Montenegro: "40-49.9%",
+		Canada: "50-59.9%",
+		Georgia: "40-49.9%",
+		Hungary: "50-59.9%",
+		Brazil: "40-49.9%",
+		France: "50-59.9%",
+		"New Zealand": "50-59.9%",
+		Ireland: "50-59.9%",
+		Switzerland: "50-59.9%",
+		Macedonia: "40-49.9%",
+		Finland: "40-49.9%",
+		Slovenia: "40-49.9%",
+		Serbia: "40-49.9%",
+		Albania: "40-49.9%",
+		Philippines: "40-49.9%",
+		Poland: "40-49.9%",
+		Latvia: "40-49.9%",
+		"Czech Republic": "40-49.9%",
+		Jamaica: "40-49.9%",
+		Mexico: "40-49.9%",
+		"Dominican Republic": "40-49.9%",
+		Moldova: "40-49.9%",
+		Lithuania: "40-49.9%",
+		"Costa Rica": "40-49.9%",
+		Cyprus: "40-49.9%",
+		"United Arab Emirates": "30-39.9%",
+		Nicaragua: "30-39.9%",
+		Italy: "30-39.9%",
+		Bangladesh: "30-39.9%",
+		Chile: "30-39.9%",
+		Israel: "30-39.9%",
+		"Bosnia and 57.41": "30-39.9%",
+		Uruguay: "30-39.9%",
+		Colombia: "30-39.9%",
+		Kosovo: "30-39.9%",
+		Slovakia: "30-39.9%",
+		Nigeria: "30-39.9%",
+		Uganda: "30-39.9%",
+		Guatemala: "30-39.9%",
+		Nepal: "30-39.9%",
+		Turkey: "30-39.9%",
+		Paraguay: "30-39.9%",
+		Ukraine: "30-39.9%",
+		Armenia: "30-39.9%",
+		Ecuador: "30-39.9%",
+		Vietnam: "30-39.9%",
+		Bulgaria: "30-39.9%",
+		Peru: "30-39.9%",
+		"South Africa": "30-39.9%",
+		"Sierra Leone": "30-39.9%",
+		Iceland: "30-39.9%",
+		Jordan: "30-39.9%",
+		"Sri Lanka": "30-39.9%",
+		Thailand: "30-39.9%",
+		Mozambique: "30-39.9%",
+		Indonesia: "30-39.9%",
+		Benin: "30-39.9%",
+		Romania: "30-39.9%",
+		India: "30-39.9%",
+		Senegal: "30-39.9%",
+		"Trinidad and Tobago": "20-29.9%",
+		Taiwan: "20-29.9%",
+		Estonia: "20-29.9%",
+		Pakistan: "20-29.9%",
+		Egypt: "20-29.9%",
+		Panama: "20-29.9%",
+		Azerbaijan: "20-29.9%",
+		"Burkina Faso": "20-29.9%",
+		Qatar: "20-29.9%",
+		Greece: "20-29.9%",
+		"El Salvador": "20-29.9%",
+		Rwanda: "20-29.9%",
+		Lesotho: "20-29.9%",
+		Tajikistan: "20-29.9%",
+		Cambodia: "20-29.9%",
+		Malaysia: "20-29.9%",
+		Ethiopia: "20-29.9%",
+		Bolivia: "20-29.9%",
+		Guyana: "20-29.9%",
+		Barbados: "20-29.9%",
+		Tunisia: "20-29.9%",
+		Djibouti: "20-29.9%",
+		Belarus: "20-29.9%",
+		Cameroon: "20-29.9%",
+		Lebanon: "20-29.9%",
+		Honduras: "20-29.9%",
+		Luxembourg: "20-29.9%",
+		Myanmar: "20-29.9%",
+		"Ivory Coast": "20-29.9%",
+		Kyrgyzstan: "20-29.9%",
+		Zambia: "20-29.9%",
+		Kazakhstan: "20-29.9%",
+		China: "20-29.9%",
+		Haiti: "20-29.9%",
+		Gambia: "20-29.9%",
+		Namibia: "20-29.9%",
+		Bahrain: "20-29.9%",
+		Mongolia: "20-29.9%",
+		Mauritius: "20-29.9%",
+		Laos: "20-29.9%",
+		Japan: "20-29.9%",
+		Liberia: "20-29.9%",
+		Oman: "20-29.9%",
+		Swaziland: "20-29.9%",
+		Gabon: "20-29.9%",
+		Tanzania: "20-29.9%",
+		"United Republic of Tanzania": "20-29.9%",
+		Chad: "20-29.9%",
+		Venezuela: "20-29.9%",
+		"Saudi Arabia": "20-29.9%",
+		"South Korea": "20-29.9%",
+		Ghana: "20-29.9%",
+		Russia: "20-29.9%",
+		Algeria: "20-29.9%",
+		Madagascar: "20-29.9%",
+		Singapore: "20-29.9%",
+		Uzbekistan: "20-29.9%",
+		Botswana: "20-29.9%",
+		Turkmenistan: "20-29.9%",
+		Mauritania: "20-29.9%",
+		Kuwait: "20-29.9%",
+		Suriname: "20-29.9%",
+		Malawi: "20-29.9%",
+		Burundi: "20-29.9%",
+		Kenya: "20-29.9%",
+		Morocco: "20-29.9%",
+		"Timor-Leste": "20-29.9%",
+		Niger: "20-29.9%",
+		"Republic of the Congo": "20-29.9%",
+		Cuba: "20-29.9%",
+		Mali: "20-29.9%",
+		Sudan: "20-29.9%",
+		Angola: "20-29.9%",
+		Zimbabwe: "20-29.9%",
+		"Guinea-Bissau": "20-29.9%",
+		Brunei: "10-19.9%",
+		"Cape Verde": "10-19.9%",
+		Togo: "10-19.9%",
+		"South Sudan": "10-19.9%",
+		"Democratic Republic of the Congo": "10-19.9%",
+		Guinea: "10-19.9%",
+		"Papua New Guinea": "10-19.9%",
+		"CentralAfrican Republic": "10-19.9%",
+		"Hong Kong, SAR China": "10-19.9%",
+		"Equatorial Guinea": "<0-9.9%",
+		Eritrea: "<0-9.9%",
+		Iran: "<0-9.9%",
+		"North Korea": "<0-9.9%",
+	},
+];
 
-	// Append the svg object to the body of the page
-	const svg = d3
-		.select("svg")
-		.append("g")
-		.attr("transform", `translate(${margin.left},${margin.top})`);
+let mapIndex = 0;
 
-	// Reshape the data
-	let reshapedData = [];
-	data.forEach((d) => {
-		for (let year = 2019; year <= 2023; year++) {
-			reshapedData.push({
-				name: d.name,
-				year: year,
-				value: +d[year + " s1"],
-			});
-		}
-	});
+const connections = [
+	{
+		company: "H&M",
+		companyLocation: "Stockholm, Sweden",
+		supplierCountry: "Bangladesh",
+	},
+];
 
-	// Group the data by name
-	let groupedData = d3.group(reshapedData, (d) => d.name);
+// Geocoded locations (latitude and longitude)
+const locations = {
+	"Stockholm, Sweden": { lat: 59.3293, lon: 18.0686 },
+	Bangladesh: { lat: 23.685, lon: 90.3563 },
+};
 
-	// Color scale
-	const color = d3
-		.scaleOrdinal()
-		.domain(groupedData.keys())
-		.range(d3.schemeCategory10);
+// Function to project coordinates to the map
+function projectPoint(lat, lon) {
+	return projection([lon, lat]);
+}
 
-	// X scale and axis
-	const x = d3.scaleLinear().domain([2019, 2023]).range([0, width]);
+const width = 960,
+	height = 600;
+
+const projection = d3
+	.geoMercator()
+	.scale(130)
+	.translate([width / 2, height / 1.5]);
+
+const path = d3.geoPath().projection(projection);
+
+const svg = d3
+	.select("#map")
+	.append("svg")
+	.attr("width", width)
+	.attr("height", height);
+
+const tooltip = d3.select(".tooltip");
+
+function getColorForRating(rating) {
+	if (!rating) {
+		return "#D3D3D3"; // Color for countries not in the data
+	}
+	switch (rating) {
+		case "60-69.9%":
+			return "#FFF2D6";
+		case "50-59.9%":
+			return "#FFD160";
+		case "40-49.9%":
+			return "#FEB854";
+		case "30-39.9%":
+			return "#FF8B27";
+		case "20-29.9%":
+			return "#FF5200";
+		case "10-19.9%":
+			return "#810135";
+		case "<0-9.9%":
+			return "#810135";
+		default:
+			return "#D3D3D3";
+	}
+}
+
+d3.json(
+	"https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson"
+).then((data) => {
 	svg
-		.append("g")
-		.attr("transform", `translate(0,${height})`)
-		.call(d3.axisBottom(x).tickFormat(d3.format("d")));
-
-	// Y scale and axis
-	const y = d3
-		.scaleLinear()
-		.domain([0, d3.max(reshapedData, (d) => d.value)])
-		.range([height, 0]);
-	svg.append("g").call(d3.axisLeft(y));
-
-	// Line generator
-	const line = d3
-		.line()
-		.x((d) => x(d.year))
-		.y((d) => y(d.value));
-
-	// Draw the lines
-	svg
-		.selectAll(".line")
-		.data(groupedData)
+		.selectAll(".country")
+		.data(data.features)
 		.enter()
 		.append("path")
-		.attr("class", "line")
-		.attr("d", (d) => line(d[1]))
-		.attr("fill", "none")
-		.attr("stroke", (d) => color(d[0]))
-		.attr("stroke-width", 1.5)
-		.on("mouseover", function (event, d) {
-			tooltip.transition().duration(200).style("opacity", 1);
-			tooltip
-				.html(d[0]) // Displaying the name on the tooltip
-				.style("left", event.pageX + "px")
-				.style("top", event.pageY + "px");
+		.attr("class", "country")
+		.attr("d", path)
+		.style("fill", (d) => {
+			// Assign a color based on a random score
+			const rating = mapData[mapIndex][d.properties.name];
+			return getColorForRating(rating);
 		})
-		.on("mousemove", function (event, d) {
+		.on("mouseover", function (event, d) {
+			tooltip.transition().duration(200).style("opacity", 0.9);
 			tooltip
+				.html(
+					d.properties.name +
+						"<br/>GSI Rating: " +
+						(mapData[mapIndex][d.properties.name] || "not rated")
+				)
 				.style("left", event.pageX + "px")
-				.style("top", event.pageY + "px");
+				.style("top", event.pageY - 28 + "px");
 		})
 		.on("mouseout", function (d) {
 			tooltip.transition().duration(500).style("opacity", 0);
 		});
 
-	// Tooltip
-	const tooltip = d3
-		.select("body")
-		.append("div")
-		.attr("class", "tooltip")
-		.style("opacity", 0)
-		.style("background-color", "white")
-		.style("border", "solid")
-		.style("border-width", "2px")
-		.style("border-radius", "5px")
-		.style("padding", "5px")
-		.style("position", "absolute");
+	connections.forEach((conn) => {
+		const companyCoords = projectPoint(
+			locations[conn.companyLocation].lat,
+			locations[conn.companyLocation].lon
+		);
+		const supplierCoords = projectPoint(
+			locations[conn.supplierCountry].lat,
+			locations[conn.supplierCountry].lon
+		);
+
+		// Draw company point
+		const companyPoint = svg
+			.append("circle")
+			.attr("cx", companyCoords[0])
+			.attr("cy", companyCoords[1])
+			.attr("r", 2)
+			.style("fill", "blue");
+
+		const supplierPoint = svg
+			.append("circle")
+			.attr("cx", supplierCoords[0])
+			.attr("cy", supplierCoords[1])
+			.attr("r", 2)
+			.style("fill", "blue");
+
+		// Tooltip for company
+		companyPoint
+			.on("mouseover", function (event) {
+				tooltip.transition().duration(200).style("opacity", 0.9);
+				tooltip
+					.html(conn.company + "<br/> HQ: " + conn.companyLocation)
+					.style("left", event.pageX + "px")
+					.style("top", event.pageY - 28 + "px");
+			})
+			.on("mouseout", function () {
+				tooltip.transition().duration(500).style("opacity", 0);
+			});
+
+		// Draw line connecting company and supplier
+		const connectionLine = svg
+			.append("line")
+			.attr("x1", companyCoords[0])
+			.attr("y1", companyCoords[1])
+			.attr("x2", supplierCoords[0])
+			.attr("y2", supplierCoords[1])
+			.style("stroke", "black")
+			.style("stroke-width", 2);
+		connectionLine
+			.on("mouseover", function (event) {
+				tooltip.transition().duration(200).style("opacity", 0.9);
+				tooltip
+					.html(conn.company + "<br/> HQ: " + conn.companyLocation)
+					.style("left", event.pageX + "px")
+					.style("top", event.pageY - 28 + "px");
+			})
+			.on("mouseout", function () {
+				tooltip.transition().duration(500).style("opacity", 0);
+			});
+	});
 });
+
+document.getElementById("dataSelector").addEventListener("change", function () {
+	// Update the global mapIndex based on user selection
+	mapIndex = parseInt(this.value, 10); // Convert string value to integer
+	console.log(mapIndex);
+	updateMap(); // Call the function to update the map
+});
+
+function updateMap() {
+	svg
+		.selectAll(".country")
+		.style("fill", function (d) {
+			// Use the global mapIndex to determine the fill color
+			const rating = mapData[mapIndex][d.properties.name];
+			return getColorForRating(rating);
+		})
+		.on("mouseover", function (event, d) {
+			tooltip.transition().duration(200).style("opacity", 0.9);
+			tooltip
+				.html(
+					d.properties.name +
+						"<br/>GSI Rating: " +
+						(mapData[mapIndex][d.properties.name] || "not rated")
+				)
+				.style("left", event.pageX + "px")
+				.style("top", event.pageY - 28 + "px");
+		})
+		.on("mouseout", function (d) {
+			tooltip.transition().duration(500).style("opacity", 0);
+		});
+}
